e70a9fcd793dbd73c77893674ba5333acefdc6df
Hamon Giacomelli <hamongtgiacomelli@gmail.com>
2020-10-11 18:26:11 -0300

    Adding prettier

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 class App {
-    public express: express.Application
+  public express: express.Application;
 
-    public constructor() {
-        this.express = express();
+  public constructor() {
+    this.express = express();
 
-        this.middleWares();
-        this.database();
-        this.routes();
-    }
+    this.middleWares();
+    this.database();
+    this.routes();
+  }
 
-    private middleWares (): void {
-        this.express.use(express.json());
-        this.express.use(cors());
-    }
+  private middleWares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
 
-    private database (): void {
-        mongoose.connect('mongodb://localhost:27017/guilder', {
-            useNewUrlParser: true
-        })
-    }
+  private database(): void {
+    mongoose
+      .connect("mongodb://localhost:27017/guilder", {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      })
+      .then(() => console.log("DB Connected!"))
+      .catch((err) => {
+        console.log(`DB Connection Error: ${err.message}`);
+      });
+  }
 
-    private routes(): void {
-        this.express.get('/', (req, res) => {
-            res.send('Hello World');
-        })
-    }
+  private routes(): void {
+    this.express.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+  }
 }
 
 export default new App().express;
